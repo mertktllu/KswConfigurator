9cfcdfd6644ec47982132b4d3781ce432bd9a1b8
bhdrsnlms <c1911060@student.cankaya.edu.tr>
2024-04-22 22:56:30 +0300

    delete old admin operations

--- MAN-front-proto/src/views/Home.vue
+++ MAN-front-proto/src/views/Home.vue
@@ -100,40 +100,6 @@
             </v-card>
           </v-dialog>
         </v-col>
-        <!-- <v-col>
-          <v-dialog max-width="500">
-            <template v-slot:activator="{ props: activatorProps }">
-              <v-btn v-bind="activatorProps" text="Login As Admin"> </v-btn>
-            </template>
-            <template v-slot:default="{ isActive }">
-              <v-card>
-                <v-img
-                  src="https://upload.wikimedia.org/wikipedia/commons/5/54/Logo_MAN.png"
-                ></v-img>
-                <v-card-text>
-                  <v-text-field
-                    label="Email"
-                    variant="solo-filled"
-                  ></v-text-field>
-                  <v-text-field
-                    label="Password"
-                    variant="solo-filled"
-                  ></v-text-field>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green" text="Login" @click="goAdmin()"></v-btn>
-
-                  <v-btn
-                    color="red"
-                    text="Close"
-                    @click="isActive.value = false"
-                  ></v-btn>
-                </v-card-actions>
-              </v-card>
-            </template>
-          </v-dialog>
-        </v-col> -->
       </v-row>
     </v-card>
   </div>
