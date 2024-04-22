c0a19e78e69c11c00ee777ea852f49e2375e41a5
bhdrsnlms <c1911060@student.cankaya.edu.tr>
2024-04-22 22:49:39 +0300

    Admin's login process has been fixed.

--- MAN-front-proto/src/views/Home.vue
+++ MAN-front-proto/src/views/Home.vue
@@ -56,6 +56,51 @@
         </v-col>
 
         <v-col>
+          <v-dialog max-width="500" v-model="dialogAdmin">
+            <template v-slot:activator="{ props: activatorProps }">
+              <v-btn v-bind="activatorProps" text="Login As ADMIN"> </v-btn>
+            </template>
+
+            <v-card>
+              <v-img
+                src="https://upload.wikimedia.org/wikipedia/commons/5/54/Logo_MAN.png"
+              ></v-img>
+              <v-form
+                ref="form"
+                v-model="valid"
+                @submit.prevent="validateAdmin"
+              >
+                <v-card-text>
+                  <v-text-field
+                    v-model="email"
+                    label="Email"
+                    variant="solo-filled"
+                    :rules="emailRules"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    label="Password"
+                    variant="solo-filled"
+                    :type="showPassword ? 'text' : 'password'"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword"
+                    :rules="passwordRules"
+                    required
+                  ></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn type="submit" color="primary">Continue As Admin</v-btn>
+                  <v-btn color="red" text @click="closeDialogAdmin"
+                    >Close</v-btn
+                  >
+                </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-dialog>
+        </v-col>
+        <!-- <v-col>
           <v-dialog max-width="500">
             <template v-slot:activator="{ props: activatorProps }">
               <v-btn v-bind="activatorProps" text="Login As Admin"> </v-btn>
@@ -88,7 +133,7 @@
               </v-card>
             </template>
           </v-dialog>
-        </v-col>
+        </v-col> -->
       </v-row>
     </v-card>
   </div>
@@ -101,6 +146,7 @@ export default {
   data() {
     return {
       dialog: false,
+      dialogAdmin: false,
       email: "",
       password: "",
       valid: true,
@@ -118,6 +164,11 @@ export default {
         "user1@example.com": "password1",
         "user2@example.com": "password2",
         "user3@example.com": "password3",
+      },
+      admins: {
+        "admin1@example.com": "password1",
+        "admin2@example.com": "password2",
+        "admin3@example.com": "password3",
       },
     };
   },
@@ -143,6 +194,17 @@ export default {
         alert("Incorrect username or password."); // Yanlış bilgi uyarısı
       }
     },
+    validateAdmin() {
+      if (
+        this.$refs.form.validate() &&
+        this.admins[this.email] === this.password
+      ) {
+        this.$router.push("/admin"); // Doğru bilgilerle giriş yapıldı
+        this.dialogAdmin = false; // Dialog'u kapat
+      } else {
+        alert("Incorrect username or password."); // Yanlış bilgi uyarısı
+      }
+    },
     checkCredentials() {
       if (this.users[this.email] && this.users[this.email] === this.password) {
         this.$router.push("/customer");
@@ -153,6 +215,9 @@ export default {
     },
     closeDialog() {
       this.dialog = false; // Dialog'u kapat
+    },
+    closeDialogAdmin() {
+      this.dialogAdmin = false;
     },
   },
 };
