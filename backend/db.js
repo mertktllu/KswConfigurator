0d2f141f9ab093dcb25f18f936b1ecadd1cc2080
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-19 14:35:52 +0300

    xx
    xx

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,33 @@
-const sql = require('mssql');
+const { Connection, Request } = require('tedious');
 
+// Config settings for the SQL server
 const config = {
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
   server: process.env.DATABASE_SERVER,
-  database: process.env.DATABASE_NAME,
-  driver: process.env.DATABASE_DRIVER,
-  port: parseInt(process.env.DATABASE_PORT),
-  options: {
-    encrypt: false, // true if you're connecting to an Azure SQL database
-    enableArithAbort: true,
+  authentication: {
+    type: 'default',
+    options: {
+      userName: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+    }
   },
+  options: {
+    database: process.env.DATABASE_NAME,
+    encrypt: true,
+    rowCollectionOnDone: true,
+    trustServerCertificate: true,
+  }
 };
 
-sql.connect(config, (err) => {
+// Create connection to database
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', err => {
   if (err) {
-    console.log('Database Connection Failed! Bad Config: ', err);
+    console.error('Database Connection Failed! Bad Config: ', err);
   } else {
     console.log('Connected to the database');
   }
 });
+
+module.exports = connection;
