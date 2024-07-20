3d030125acdf3b8e219ed0ad5e552782e898c1ef
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-21 00:40:20 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,22 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_SERVER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASS,
+  port: process.env.DATABASE_PORT,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+pool.connect((err) => {
+  if (err) {
+    console.error('Database connection error', err.stack);
+  } else {
+    console.log('Database connected successfully');
+  }
+});
+
+module.exports = pool;
