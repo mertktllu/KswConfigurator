fcc55e6c2724ac046c520ab3af59022eaadcf9bd
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-21 00:33:28 +0300

    xx
    xx

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,13 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// Config settings for the PostgreSQL server
-const config = {
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_SERVER,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASS,
-  port: process.env.DATABASE_PORT
-};
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-// Create a pool of connections
-const pool = new Pool(config);
-
-// Export the pool object for use in other modules
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
