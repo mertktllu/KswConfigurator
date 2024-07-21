1b1d6bdbfdb194933e762c57b406a6c47d282dfa
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-21 17:12:38 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+const path = require('path');
+
 require('dotenv').config();
 
 const app = express();
