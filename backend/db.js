917f9a9a9ba333bafeed08832781dde0dda01af8
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-21 21:22:08 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,9 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = process.env.DATABASE_URL;
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 pool.connect((err) => {
