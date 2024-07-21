70fc0fde4f8e655eb6b9d3834681663862f6feeb
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-21 16:45:02 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
-const express = require("express");
-const db = require("./db");
-const path = require("path");
-const cors = require("cors"); // CORS paketini dahil edin ve sadece bir kez tanımlayın
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.use(cors(corsOptions)); // CORS middleware'ini kullanın
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(express.json());
