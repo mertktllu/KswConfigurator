7053f4b11032840f6b26a93b26f5719bb08aa1bd
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-21 16:22:33 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
