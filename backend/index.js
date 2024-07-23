27c65e18fdc9588c1684de0482e540c61fa3bb5f
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-23 13:45:18 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -497,7 +497,7 @@ app.post("/denyRequest/:id", async (req, res) => {
 });
 
 
-
+app.use(express.static(path.join(__dirname, '../dist')));
 app.use('/assets', express.static(path.join(__dirname, '../dist/assets')));
 app.use('/static', express.static(path.join(__dirname, '../dist/static')));
 
