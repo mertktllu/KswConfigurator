3f057912abab72bf0316d23821ff41e751c44888
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-24 18:45:28 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Statik dosyaları sunmak
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 
 
