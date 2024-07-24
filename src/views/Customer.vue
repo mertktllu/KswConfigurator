750a823607c70b2ce1f92bf01c22f2a3bfc0e4c9
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-25 01:28:49 +0300

    Update Customer.vue

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -4,7 +4,7 @@
     <v-row>
       <v-col class="d-flex justify-start">
         <v-img
-          src="https://mandb.s3.eu-north-1.amazonaws.com/normal.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/5/54/Logo_MAN.png"
           contain
           max-height="60"
           max-width="100"
@@ -505,7 +505,7 @@
 
             <!-- Bestuhlung -->
             <img
-  v-else-if="selectedMainGroup?.Name === 'Bestuhlung'"
+   v-else-if="selectedMainGroup?.name.trim() === 'Bestuhlung'"
   src="https://mandb.s3.eu-north-1.amazonaws.com/normal.png"
   style="width: 120%; height: auto; display: block; bottom: auto"
 ></img>
