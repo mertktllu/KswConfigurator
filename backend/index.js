9b9ceb288421bc9c902a577445cf95cf53ed94c2
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-07-24 15:12:13 +0300

    xx
    xx

--- backend/index.js
+++ backend/index.js
@@ -497,12 +497,12 @@ app.post("/denyRequest/:id", async (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./dist/index.html"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
