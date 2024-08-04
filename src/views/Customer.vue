2330b1c7f20eff4be7cbec0648bbe0e69e87098a
mertktllu <67332036+mertktllu@users.noreply.github.com>
2024-08-04 15:36:12 +0300

    Update Customer.vue

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -2312,11 +2312,11 @@ export default {
           if (this.selectedModel["Topcloser"]) {
             this.accumulatedDetails.push({
               text: this.selectedModel["Topcloser"],
-              position: { top: "3%", left: "30%" },
+              position: { top: "0%", left: "30%" },
               color: this.getRalColor(this.selectedModel["Topcloser"]),
               showArrow: true,
-              lineStart: { top: "1%", left: "30%" },  // Çizginin başladığı nokta
-              lineEnd: { top: "5%", left: "30%" }   
+              lineStart: { top: "2%", left: "15%" },  // Çizginin başladığı nokta
+              lineEnd: { top: "10%", left: "10%" }   
             });
             
           
